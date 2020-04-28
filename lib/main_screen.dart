ee2bcd5316db71010e4e2cc19fd8fb76f93086ed
radhikasethi2011 <radhika.sethi0312@gmail.com>
2020-04-28 17:01:52 +0530

    adding a card, questions, ticks

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
@@ -37,12 +38,92 @@ class _MainScreenState extends State<MainScreen> {
           ),
         ),
       ) ,
-      body: FutureBuilder(
-        future: fetchQuestions(),
-        builder: (BuildContext context, AsyncSnapshot snapshot){
+      body: RefreshIndicator(
+        onRefresh: fetchQuestions,
+              child: FutureBuilder(
+          future: fetchQuestions(),
+          builder: (BuildContext context, AsyncSnapshot snapshot){
+              if(snapshot.connectionState == ConnectionState.done)
+              {
+                return Container(
+                  child: getQuestionsList(),
 
-        },
+                );
+              }
+              else{
+                return Center(
+                  child:CircularProgressIndicator(),
+                );
+              }
+          },
+        ),
       ),
       );
+  }//buld method ends. 
+  ListView getQuestionsList()//returning a list view of widgets
+  {
+    return ListView.builder( //builder increments the index for the whole length of the widgets. (for itemCount length)
+      itemCount: results.length, //total items = no of results. 
+      itemBuilder: (BuildContext context, int index){
+        return Card(
+          child: ExpansionTile(
+            title : ListTile(
+              title : Text(results[index].question, 
+              style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle : Text(results[index].category, 
+              style: TextStyle(fontStyle: FontStyle.italic),
+              ),
+
+            ),
+            children: results[index].allAnswers.map((option)//now for displaying all the answers. mapping List to widget
+            {
+              return AnswerWidget(results, index, option);
+              
+            }).toList(),
+
+            
+          ),
+        );
+      },
+    );
+  }
+}
+
+class AnswerWidget  extends StatefulWidget {
+  final List<Results> results;
+  final int index;
+  final String option;
+  AnswerWidget(this.results, this.index, this.option);
+  @override 
+  _AnswerWidgetState createState() =>  _AnswerWidgetState();
+
+  
+}
+
+class _AnswerWidgetState extends State<AnswerWidget> {
+  @override
+  Color tick;
+  Widget build(BuildContext context) {
+      return ListTile(
+      onTap: (){
+       
+       setState(() {
+          if(widget.option == widget.results[widget.index].correctAnswer)
+        {
+          tick = Colors.green;
+        }
+        else
+        {
+          tick = Colors.red;
+        }
+       });
+      },
+        title: Text(
+          widget.option, 
+          textAlign: TextAlign.center, 
+          style: TextStyle(color: tick)), 
+      );
+    
   }
 }
